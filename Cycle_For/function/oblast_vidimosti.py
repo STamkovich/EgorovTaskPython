28b1a2aa422b0fb68db57b2adffdcc6db304ebd1
Sergey Tamkovich <tamkovich1072@gmail.com>
2021-09-26 05:58:17 +0300

    "Область видимости"

--- Cycle_For/function/oblast_vidimosti.py
+++ Cycle_For/function/oblast_vidimosti.py
@@ -103,3 +103,13 @@ i()
 # MIN_DRIVING_AGE = 18
 # allowed_driving('tim', 17) # выведет "tim еще рано садиться за руль"
 # allowed_driving('bob', 18) # выведет "bob может водить"
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    global MIN_DRIVING_AGE
+    if age >= MIN_DRIVING_AGE:
+        return print(f'{name} может водить')
+    return print(f'{name} еще рано садиться за руль')
+
+allowed_driving('bob', 18)
